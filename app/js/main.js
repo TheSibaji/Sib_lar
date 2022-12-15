fbc711c78840600288ba015316a0bd1f1ccf76b8
Sibaji Khillar <103411781+TheSibaji@users.noreply.github.com>
2022-12-15 09:58:55 +0530

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-input",{
+    strings:["Student.","Frontend Web Developer.","Ethical Hacker.","Photographer."],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
